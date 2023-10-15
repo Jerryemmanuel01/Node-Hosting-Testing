42b25c364463a9ad181f87b6d2b3a2cf14d30858
Jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2023-10-15 20:56:59 +0100

    cors added

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+app.use(cors());
 
 app.use("/", (req, res) => {
   res.send("Server is running on port 5000");
